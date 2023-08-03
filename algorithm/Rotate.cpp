f0c73a37a097b1af3237ee041454954b3cbef23a
tnear <11488488+tnear@users.noreply.github.com>
2023-08-03 17:45:11 -0500

    Rotate, StablePartition

---
+++ algorithm/Rotate.cpp
@@ -0,0 +1,55 @@
+/*
+https://cplusplus.com/reference/algorithm/rotate/
+Rotates the order of the elements in the range [first,last), in such a way
+that the element pointed by middle becomes the new first element.
+*/
+
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+// https://hackingcpp.com/cpp/std/algorithms/reordering.html#rotate
+void seanParent()
+{
+    // [0  1  2  3  4  5  6  7  8]
+    //  ^begin   ^newFirst   ^end
+    vector<int> v = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+
+    auto begin = v.begin();        // where to start rotating (anything to the left is untouched)
+    auto newFirst = v.begin() + 3; // the element which slides to 'begin'
+    auto end = v.end() - 2;        // one past the last touched element (end is untouched)
+
+    assert(*begin == 0);
+    assert(*newFirst == 3);
+    assert(*end == 7);
+
+    auto newFirstLoc = std::rotate(begin, newFirst, end);
+
+    // std::rotate creates:
+    // [3  4  5  6  0  1  2  7  8]
+    //              ^newFirst
+    vector<int> expVec = {3, 4, 5, 6, 0, 1, 2, 7, 8};
+    assert(v == expVec);
+
+    // return value points to element which used to be the beginning
+    assert(*newFirstLoc == 0);         // remains zero
+    assert(newFirstLoc == v.begin() + 4); // moves location
+}
+
+void test()
+{
+    seanParent();
+}
+
+int main()
+{
+    test();
+
+    std::cout << std::endl <<  __FILE__ " tests passed!" << std::endl;
+    return 0;
+}
---
+++ algorithm/StablePartition.cpp
@@ -0,0 +1,61 @@
+/*
+https://cplusplus.com/reference/algorithm/stable_partition/
+Rearranges the elements in the range [first, last), in such a way
+that all the elements for which pred returns true precede all
+those for which it returns false, and, unlike function partition,
+the relative order of elements within each group is preserved.
+*/
+
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+bool isOdd(int x)
+{
+    return x % 2;
+}
+
+// https://hackingcpp.com/cpp/std/algorithms.html#stable_partition
+void seanParent()
+{
+    // [0  1  2  3  4  5  6  7  8]
+    //        ^begin      ^end
+    vector<int> v = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+
+    auto begin = v.begin() + 2; // where to start partitioning (anything to the left is untouched)
+    auto end = v.begin() + 6;   // one past the last touched element (end is untouched)
+
+    // the unary predicate groups elements which return TRUE on the left and FALSE on the right
+    // therefore, the odd elements move left and even move right
+
+    auto partitionPoint = std::stable_partition(begin, end, isOdd);
+
+    // stable_partition creates:
+    // [0  1  3  5  2  4  6  7  8]
+    //              ^partitionPoint
+    vector<int> expVec = {0, 1, 3, 5, 2, 4, 6, 7, 8};
+    assert(v == expVec);
+
+    // partition return value points to 1st elem in 2nd group where predicate returns FALSE
+    // for this data, 2 is the first even number in the [begin, end) range
+    assert(*partitionPoint == 2);
+    assert(partitionPoint == v.begin() + 4);
+}
+
+void test()
+{
+    seanParent();
+}
+
+int main()
+{
+    test();
+
+    std::cout << std::endl <<  __FILE__ " tests passed!" << std::endl;
+    return 0;
+}
